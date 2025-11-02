cfea2c93903af6dff1aafe82eccdce83d4482cbe
ha5804 <zixpotf5803@gmail.com>
2025-11-02 11:18:15 +0900

    definition convoulution

--- model.py
+++ model.py
@@ -1,10 +1,30 @@
 import numpy as np
 
 class CNN:
-    def __init__(self, filter_size = (5,5), eps = 1e-9):
-        self.filter_size = filter_size
-        self.weight = np.zeros(filter_size) + eps
+    def __init__(self,kernel_size = (3,3), eps = 1e-9):
+        self.kernel = np.zeros((kernel_size)) + eps
         
+    
+    def convolution(self, input, stride = 1):
+        batch_size , H, W = input.shape
+        kh, kw = self.kernel.shape
+
+        out_h = (H - kh) // stride + 1
+        out_w = (W - kw) // stride + 1
+
+        outputs = np.zeros((batch_size, out_h, out_w))
+
+        for b, img in enumerate(input):
+            for j in range(0, out_h):
+                for k in range(0, out_w):
+                    region = img[j* stride : j * stride +kh,
+                               k * stride : k * stride + kw]
+                    
+                    outputs[b,j,k] = np.sum(region * self.kernel)
+        
+        return outputs
+        
+
 
 
 
